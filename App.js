96d0c68a121ea47523df66ca257ba8b7d97dd14f
frantoqui <franz.torrez@bolcore.com>
2019-02-06 21:47:52 +0800

    aaa

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Test
+            Learn React
           </a>
         </header>
       </div>
