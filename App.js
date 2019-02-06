4ee47bddfa924937ba3fd37eb58ec783660ccf5b
frantoqui <franz.torrez@bolcore.com>
2019-02-06 23:37:54 +0800

    aaaaaaaaa

--- App.js
+++ App.js
@@ -9,52 +9,21 @@ import { Link, Route, Switch } from 'react-router-dom';
 /*Pages*/
 import github from './github.js'
 
-/* Home component */
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-)
-
-/* Category component */
-const Category = () => (
-  <div>
-    <h2>Category</h2>
-  </div>
-)
-
-/* Products component */
-const Products = () => (
-  <div>
-    <h2>Products</h2>
-  </div>
-)
-
 /* App component */
 class App extends Component {
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-light">
-          <ul className="nav navbar-nav">
-
-           /* Link components are used for linking to other views */
-            <li><Link to="/">Homes</Link></li>
-            <li><Link to="/category">Category</Link></li>
-            <li><Link to="/products">Products</Link></li>
-
-          </ul>
-         </nav>
-
-          /* Route components are rendered if the path prop matches the current URL */
-           <Route path="/" component={Home}/>
-           <Route path="/category" component={Category}/>
-           <Route path="/products" component={Products}/>
-
-      </div>
-    )
-  }
+      <BrowserRouter>
+        <div>
+          <Route exact={true} path='/' render={() => (
+            <div className="App">
+              <github />
+            </div>
+          )}/>
+		    </div>
+		  </BrowserRouter>
+		);
+	}
 }
-
 
 export default App;
