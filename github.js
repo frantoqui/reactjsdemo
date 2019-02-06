93e4a0a9be4ff458bd785617e16edf391bcb6f9d
frantoqui <franz.torrez@bolcore.com>
2019-02-07 00:17:35 +0800

    asdasd

--- github.js
+++ github.js
@@ -9,8 +9,8 @@ export default class Github extends Component {
   	const name = 'Josh Perez';
 		const element = <h1>Hello, {name}</h1>;
 		
-		var shell_exec = require('shell_exec').shell_exec;
-		var result = shell_exec('git pull origin master');
+		//var shell_exec = require('shell_exec').shell_exec;
+		//var result = shell_exec('git pull origin master');
   	const script = document.createElement("script");
   	script.src = "console.log(result);";
     script.async = true;
