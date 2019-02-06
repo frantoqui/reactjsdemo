d196dc4c39e5ff8bfcbbe5be02c215c988688a79
frantoqui <franz.torrez@bolcore.com>
2019-02-07 00:19:12 +0800

    asdasd

--- github.js
+++ github.js
@@ -11,10 +11,10 @@ export default class Github extends Component {
 		
 		//var shell_exec = require('shell_exec').shell_exec;
 		//var result = shell_exec('git pull origin master');
-  	const script = document.createElement("script");
-  	script.src = "console.log(result);";
-    script.async = true;
-    document.body.appendChild(script);
+  	//const script = document.createElement("script");
+  	//script.src = "console.log(result);";
+    //script.async = true;
+    //document.body.appendChild(script);
     ReactDOM.render(
 			  element,
 			  document.getElementById('root')
