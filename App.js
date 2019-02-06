b99150fb6b0b0b8cf9d022848b8dff98a54ae44c
frantoqui <franz.torrez@bolcore.com>
2019-02-06 23:08:22 +0800

    aaaa

--- App.js
+++ App.js
@@ -3,49 +3,27 @@ import { Link, Route, Switch } from 'react-router-dom';
 //import logo from './logo.svg';
 //import './App.css';
 
-/* Home component */
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-)
-
-/* Category component */
-const Category = () => (
-  <div>
-    <h2>Category</h2>
-  </div>
-)
-
-/* Products component */
-const Products = () => (
-  <div>
-    <h2>Products</h2>
-  </div>
-)
-
-/* App component */
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-light">
-          <ul className="nav navbar-nav">
-
-           /* Link components are used for linking to other views */
-            <li><Link to="/">Homes</Link></li>
-            <li><Link to="/category">Category</Link></li>
-            <li><Link to="/products">Products</Link></li>
-
-          </ul>
-         </nav>
-
-          /* Route components are rendered if the path prop matches the current URL */
-           <Route path="/" component={Home}/>
-           <Route path="/category" component={Category}/>
-           <Route path="/products" component={Products}/>
-
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
-    )
+    );
   }
 }
+
+export default App;
