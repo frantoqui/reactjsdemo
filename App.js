d608790d06c0a9b9e949a7adcf6d224f65ae790a
frantoqui <franz.torrez@bolcore.com>
2019-02-07 02:06:38 +0800

    asd

--- App.js
+++ App.js
@@ -3,42 +3,10 @@ import express from "express";
 import bodyparser from "body-parser";
 import cors from "cors";
 
-/* Import statements */
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter, Route} from 'react-router-dom';
 
-/*Pages*/
-import Github from './github'
 
 /*API*/
 import Dependencies from "./api/dependencies";
-
-/* App component */
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div id="root">
-          <Route exact={true} path='/' render={() => (
-            <div className="App">
-              Test111
-            </div>
-          )}/>
-          <Route exact={true} path='/github' render={() => (
-            <div className="App">
-            	Test2222
-              <Github />
-            </div>
-          )}/>
-          
-		    </div>
-		  </BrowserRouter>
-		);
-	}
-}
-
-export default App;
 
 /*We use Express for the Rest API*/
 
