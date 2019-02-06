5be530787c0e8590f58d4c6f8be5819867bae0d7
frantoqui <franz.torrez@bolcore.com>
2019-02-06 23:54:16 +0800

    DFGFDG

--- github.js
+++ github.js
@@ -3,26 +3,26 @@ import ReactDOM from 'react-dom';
 
 class github extends Component {
   render() {
+  
   	const name = 'Josh Perez';
 		const element = <h1>Hello, {name}</h1>;
-
+		
 		var shell_exec = require('shell_exec').shell_exec;
-		 
 		var result = shell_exec('git pull origin master');
-
-		ReactDOM.render(
-		  <h1>Hello, world!</h1>,
-		  document.getElementById('root')
-		);
-
-		console.log(result);
-
-
-		ReactDOM.render(
-		  element,
-		  document.getElementById('root')
-		);
   	
-    return ('OK');
+    return (
+    	ReactDOM.render(
+			  <h1>Hello, world!</h1>,
+			  document.getElementById('root')
+			);
+
+			console.log(result);
+
+
+			ReactDOM.render(
+			  element,
+			  document.getElementById('root')
+			);
+    );
   }
 }
